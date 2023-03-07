8638353ed873cfdac12222ed380aca99a7aac979
JunhoRS <126283654+JunhoRS@users.noreply.github.com>
2023-03-06 23:09:18 -0300

    add topic of first challenge

--- Challenge.py
+++ Challenge.py
@@ -1,5 +1,25 @@
 
 # Add the first challenger
 
-print ("Hola  mundo")
+# Complete the function solveMeFirst to compute the sum of two integers.
+# Example
+# Return .
+# Function Description
+# Complete the solveMeFirst function in the editor below.
+# solveMeFirst has the following parameters:
+# int a: the first value
+# int b: the second value
+# Returns
+# - int: the sum of  and 
+# Constraints
+# Sample Input
+
+def solveMeFirst(a, b):
+  return a + b
+
+num1 = int(input())
+num2 = int(input())
+
+sum = solveMeFirst(num1, num2)
+print(sum)
 
